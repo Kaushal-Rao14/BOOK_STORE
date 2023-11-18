e92406f19153261fcab1c1af8a5c23c216064fde
Kaushal-Rao <kaushald.rao14@gmail.com>
2023-11-18 09:33:05 +0530

    Add project files.

---
+++ BOOK_STORE/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BOOK_STORE.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
